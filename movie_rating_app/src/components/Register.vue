dd4812256d675dbb583710eeea63281a0b0829b6
Yuri Boyka <lilung.chao@gmail.com>
2018-10-09 16:16:59 +0800

    add tip message feedback

--- movie_rating_app/src/components/Register.vue
+++ movie_rating_app/src/components/Register.vue
@@ -22,3 +22,56 @@
     <v-btn @click="clear">clear</v-btn>
   </v-form>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: () => ({
+    valid: true,
+    name: '',
+    email: '',
+    password: '',
+    confirm_password: '',
+    emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /\S+@\S+\.\S+/.test(v) || 'E-mail must be valid',
+    ],
+  }),
+  methods: {
+    async submit () {
+      if (this.$refs.form.validate()) {
+        return axios({
+          method: 'post',
+          data: {
+            name: this.name,
+            email: this.email,
+            password: this.password,
+          },
+          url: 'http://localhost:8081/users/register',
+          headers: {
+            'Content-Type' : 'application/json',
+          },
+        })
+        .then(() => {
+          this.$swal(
+            'Great!',
+            'You have been successfully registered!',
+            'success',
+          );
+          this.$router.push({name: 'Login'});
+        })
+        .catch((error) => {
+          const message = error.response.data.message;
+          this.$swal('Oh oo!', `${message}`, 'error');
+        });
+      }
+      return true;
+    },
+    clear() {
+      this.$refs.form.reset();
+    },
+  },
+};
+</script>
+
--- movie_rating_app/src/controllers/users.js
+++ movie_rating_app/src/controllers/users.js
@@ -12,7 +12,9 @@ module.exports.controller = (app) => {
     });
     User.createUser(newUser, (error, user) => {
       if (error) {
-        console.log(error);
+        res.status(422).json({
+          message: 'Something went wrong, Please try again after scome time!',
+        })
       }
       res.send({
         user
